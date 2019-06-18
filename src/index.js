e07844f8c5ee7bf31dc5e947b5ec855260968a9f
Raphael Lima <raphael.aolima8@gmail.com>
2019-06-18 15:17:18 -0300

    chore(style): set global font style

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+      monospace;
+  }
+`;
+
+ReactDOM.render(
+  <Fragment>
+    <GlobalStyle />
+    <App />
+  </Fragment>,
+  document.getElementById('root')
+);
